5db5329fbce95ac398ce3e3f1030d8cee9f5e15f
Max41283 <max41283@mail.ru>
2022-03-22 22:28:07 +0200

    First

---
+++ src/main/java/drugsintel/accounting/GrugsintelBackendAccountingMaksBorisApplication.java
@@ -0,0 +1,13 @@
+package drugsintel.accounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrugsintelBackendAccountingMaksBorisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrugsintelBackendAccountingMaksBorisApplication.class, args);
+	}
+
+}
