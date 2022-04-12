8ca4a58acd60a599ce13db448f2b0741b2810680
Max41283 <97339720+Max41283@users.noreply.github.com>
2022-04-12 20:17:51 +0300

    Delete src directory

--- src/main/java/drugsintel/accounting/repositories/RouteRepository.java
+++
@@ -1,9 +0,0 @@
-package drugsintel.accounting.repositories;
-
-import drugsintel.accounting.models.Route;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RouteRepository extends JpaRepository<Route, Long> {
-
-
-}
